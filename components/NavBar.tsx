62105ce4954aac1fe3762c80c56936d5af3275c1
abelashine <abelashine@spin.fashion>
2025-06-24 09:51:15 +0300

    initalized web app

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/link";
+
+import { GithubIcon, HeyGenLogo } from "./Icons";
+
+export default function NavBar() {
+  return (
+    <>
+    
+    </>
+  );
+}
